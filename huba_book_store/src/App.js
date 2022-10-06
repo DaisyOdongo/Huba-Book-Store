7d70ed4d64ca2a3593e0558276a6a3465fed8a89
+ <+>
2022-10-06 05:48:22 +0300

    navabr and footer added

--- huba_book_store/src/App.js
+++ huba_book_store/src/App.js
@@ -10,14 +10,14 @@ import BookList from './components/Home/BookList';
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Routes>
         <Route path="/" element={<BookList/>} />
         <Route path="/book/:id"element ={<BooKDetails />} />
-        <Route path="/Favourites"element ={<Favourites />} />
-        <Route path="/Login"element ={<Login />} />
-        <Route path="/Navbar"element ={<Navbar />} />
-        <Route path="/Footer"element ={<Footer />} />
+        <Route path="/favourites"element ={<Favourites />} />
+        <Route path="/login"element ={<Login />} />
       </Routes>
+      <Footer />
 
       
     </div>
