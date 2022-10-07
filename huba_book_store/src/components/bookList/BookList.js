3cd4ef3d6f0b01d4142314ece3977edd1e4f4364
+ <+>
2022-10-07 13:27:42 +0300

    add books to home

---
+++ huba_book_store/src/components/bookList/BookList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "../home/Home";
+
+function BookList(){
+    return(
+        <div>
+          <Home />
+        </div>
+    )
+}
+export default BookList;
