613d85391d905654538b412d5f380842c24ed295
+ <+>
2022-10-10 10:59:05 +0300

    completed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,17 @@
 import React from "react";
-import Api, { Api_Url } from "../Global/Api";
+import SearchForm from "../searchForm/SearchForm";
 import "./Home.css"
 
 function Home(){
     
     return(
         <div className="container">
-            <div className="search">
-                <input type="text" placeholder="Search books...." />
-                <div className="btn"><button type="button"><i className="fa fa-search"></i>Search</button></div>        
-            </div>   
+            <div>
+                <SearchForm />
+            </div>
+            <div className="">
+                <div> <img src="https://images.unsplash.com/photo-1521587760476-6c12a4b040da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGlicmFyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="#"></img></div> 
+            </div>  
         </div>
     )
 }
