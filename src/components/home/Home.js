047978660498ecc6408e9274e873576aa2f1d734
+ <+>
2022-10-10 11:54:29 +0300

    completed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
-import SearchForm from "../searchForm/SearchForm";
+// import SearchForm from "../searchForm/SearchForm";
 import "./Home.css"
 
 function Home(){
     
     return(
         <div className="container">
-            <div>
+            {/* <div>
                 <SearchForm />
-            </div>
+            </div> */}
             <div className="">
                 <div> <img src="https://images.unsplash.com/photo-1521587760476-6c12a4b040da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGlicmFyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="#"></img></div> 
             </div>  
